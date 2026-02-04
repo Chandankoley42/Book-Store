68f78a4a6edbd3041e6d160136fa5a5f473571d3
Chandan Koley <chandankoley858@gmail.com>
2026-02-04 23:44:15 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://username:password@cluster0.y9lkidl.mongodb.net/?appName=Cluster0'
+export const mongoDBURL = 'mongodb+srv://ckoley1234:ckoley1234@cluster0.y9lkidl.mongodb.net/?appName=Cluster0'
 
